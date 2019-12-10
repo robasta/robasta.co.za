be723db7209c78b29bc2cce9021c6e4952919706
robasta <hello@tafadzwa.com>
2019-12-10 13:52:20 +0200

    add a sidebar component

---
+++ src/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Miniblog.Core.Services;
+
+namespace Miniblog.Core.ViewComponents
+{
+    public class SidebarViewComponent:ViewComponent
+    {
+
+        private readonly IBlogService _blog;
+        public SidebarViewComponent(IBlogService blog)
+        {
+            _blog = blog;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _blog.GetPosts(7);
+            return View( posts);
+        }
+    }
+}
